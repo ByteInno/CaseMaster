d04c98ab56b56405ae5dd8ae3197d361eccbd4c6
zero <dukepandme@gmail.com>
2023-04-12 01:11:05 +0800

    django 配置目录结构调整

---
+++ config/django/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# @Project: CaseMaster
+# @Author: zero
+# @File name: __init__.py
+# @Create time: 2023/4/10 23:08
---
+++ config/django/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# @Project: CaseMaster
+# @Author: zero
+# @File name: test
+# @Create time: 2023/4/10 23:09
+
+
+from .base import *
